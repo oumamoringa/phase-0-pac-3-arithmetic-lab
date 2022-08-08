29fcabeb2b9eafe05133ff97b1b530110847bc04
oumamoringa <richard.ouma@student.moringaschool>
2022-08-08 19:33:40 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,52 @@
+let num1 = a
+let num2 = b
+function add(){
+    return num1+=num2;
+}
+
+function subtract(){
+    return num1-=num2;
+}
+
+function multiply(){
+    return num1*=num2;
+}
+
+function divide(){
+    return num1/=num2;
+}
+
+function add(a, b){
+    return a+=b;
+}
+
+
+function subtract(a, b){
+    return a-=b;
+}
+
+function multiply(a, b){
+    return a*=b;
+}
+
+function divide(a, b){
+    return a/=b;
+}
+
+function increment(n){
+    return++n;
+}
+
+
+function decrement(n){
+    return--n;
+}
+
+
+function makeInt(n){
+   return parseInt(n, 10);
+}
+
+function  preserveDecimal(n){
+    return parseFloat(n);
+}
